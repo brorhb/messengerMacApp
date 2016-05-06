0caae51265255a513238ca370af4f0fb313d0c43
Bror <brorbrurberg@me.com>
2016-05-06 12:26:52 +0200

    no caption

--- Messenger/AppDelegate.swift
+++ Messenger/AppDelegate.swift
@@ -7,23 +7,102 @@
 //
 
 import Cocoa
+import WebKit
+import Quartz
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, WKNavigationDelegate, WKUIDelegate, NSWindowDelegate {
 
-    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
-        return true
+    // MARK: Views
+    
+    @IBOutlet var window : NSWindow!
+    @IBOutlet var view : NSView!
+    @IBOutlet var loadingView : NSImageView?
+    @IBOutlet var spinner : NSProgressIndicator!
+    @IBOutlet var longLoading : NSTextField!
+    @IBOutlet var reactivationMenuItem : NSMenuItem!
+    @IBOutlet var statusItemMenuItem : NSMenuItem!
+    @IBOutlet var toolbarTrenner : NSToolbarItem!
+    @IBOutlet var toolbarSpacing : NSToolbarItem!
+    @IBOutlet var toolbar : NSToolbar!
+
+    // MARK: Slutt
+    
+    
+    // MARK: FUNC
+    func initWindow(window: NSWindow) {
+        window.backgroundColor = NSColor.whiteColor()
+        window.minSize = NSSize(width: 380,height: 376)
+        window.makeMainWindow()
+        window.makeKeyWindow()
+        window.titlebarAppearsTransparent = true
+        window.titleVisibility = .Hidden
+        window.delegate = self
     }
+    
+    var quicklookMediaURL: NSURL? {
+        didSet {
+            if quicklookMediaURL != nil {
+                QLPreviewPanel.sharedPreviewPanel().makeKeyAndOrderFront(nil);
+            }
+        }
+    }
+    // MARK: Slutt
+    
+    
+    // MARK: Properties
+    
+    var timer : NSTimer!
+    var activatedFromBackground = false
+    var isFullscreen = false
+    
+    var statusItem = NSStatusItem()
+    var statusItemConfigurationKey = "showStatusItem"
+    var statusItemDefault = true
+    
+    // MAARK: Slutt
+    
+    
+    
+    var webView : WKWebView!
 
+    
+    
+    // MARK: NSApplication
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // initWindow
+        // initWindow(window)
     }
-
+    // MARK: Slutt
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
+    
+    
+    // MARK: Close on last open window closed
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+    // MARK: Slutt
+    
+    
+    // MARK: 
+    func changeStatusItemImage(newImage: String) {
+        if let button = statusItem.button {
+            let image = NSImage(named: newImage)
+            image!.template = true
+            button.image = image
+        }
+    }
+    // MARK: Slutt
 
-
+    // MARK: - Dock Icon
+    
+    func changeDockIcon() {
+        NSApplication.sharedApplication().applicationIconImage = NSImage(named: "Image")
+    }
+    // MARK: Slutt
 }
 
 
--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -27,3 +27,6 @@ class ViewController: NSViewController {
     }
 }
 
+
+
+
